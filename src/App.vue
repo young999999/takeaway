f72b87d3553e8d876ab34a31ddb29ae854b6e8c3
young999999 <1972780113@qq.com>
2019-09-08 20:54:22 +0800

    P9：引入vue-router

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide/>
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+  export default {
+    components: {
+      FooterGuide
+    }
+  }
+</script>
+
+<style  lang="stylus" rel="stylesheet/stylus">
+  .app
+      color :red
+</style>
